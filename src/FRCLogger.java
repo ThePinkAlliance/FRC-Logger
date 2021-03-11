595351f8cd8d5dcc7972bf90d245bc2f0372cc19
DevSamuelV <realalloutmusic@gmail.com>
2021-03-11 18:59:51 -0500

    Fixed Bufferoverflow

--- src/FRCLogger.java
+++ src/FRCLogger.java
@@ -1,73 +1,19 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
-public class FRCLogger {
-  private final String baseDir = "/home/lvuser";
-  private File file;
-  private FileWriter writer;
-  private BufferedWriter bw;
-
+public class FRCLogger extends Filemanger {
   public Csv csv;
 
-  public FRCLogger(String FileName) {
-    this.file = new File(baseDir + FileName);
-    this.csv = new Csv(FileName);
+  public FRCLogger(String file) {
+    super(file);
 
-    try {
-      writer = new FileWriter(file);
-      bw = new BufferedWriter(writer);
-    } catch (IOException err) {
-      throw new Error(err);
-    }
-  }
-
-  public boolean Create() {
-    try {
-      if (file.exists())
-        return false;
-
-      return file.createNewFile();
-    } catch (IOException err) {
-      throw new Error(err);
-    }
-  }
-
-  public boolean DestructiveCreate() {
-    try {
-      return file.createNewFile();
-    } catch (IOException err) {
-      throw new Error(err);
-    }
-  }
-
-  public void Close() {
-    try {
-      bw.close();
-      writer.close();
-    } catch (IOException err) {
-      throw new Error(err);
-    }
-  }
-
-  public void Write(Object Data) {
-    try {
-      bw.write(String.valueOf(Data));
-    } catch (IOException err) {
-      throw new Error(err);
-    }
+    csv = new Csv(file);
   }
 }
 
-class Csv {
-  private FRCLogger logger;
-
+class Csv extends Filemanger {
   public Csv(String filename) {
-    logger = new FRCLogger(filename);
+    super(filename);
   }
 
   public void LogWithTime(Object data) {
-    logger.Write(java.time.LocalTime.now().toString() + data);
+    this.Write(java.time.LocalTime.now().toString() + data);
   }
 }
